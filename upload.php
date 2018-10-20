2448c03021f51f7c6e9c66b2976e2881a7476d90
mgzam <44325804+mgzam@users.noreply.github.com>
2018-10-20 21:07:14 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 
-if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], /mnt/)) {
     echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
     " has been uploaded";
 } else{
