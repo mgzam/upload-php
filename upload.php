f203e2e4d4b1818abd2fc834b9b94d5e14eabaeb
mgzam <44325804+mgzam@users.noreply.github.com>
2018-10-22 14:02:39 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 
-$target_path = "/data/";
+$target_path = "/tmp/";
 $target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
 
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
